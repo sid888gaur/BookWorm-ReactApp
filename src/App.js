d6660e4e15ca2b5e1c212fd75d38c05fa94cd5f2
sid888gaur <siddharthgaur8@gmail.com>
2020-06-13 14:22:56 +0530

    Semantic UI and CSS

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import LoginPage from './components/pages/LoginPage'
 
 function App() {
   return (
-  	<div>
+  	<div className='ui container'>
     	<Route path="/" exact component={HomePage} />
     	<Route path="/login" exact component={LoginPage} />
 
